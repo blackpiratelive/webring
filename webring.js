d08e641b1446acfe9a1d6c1b42eb2f6e2b2a7f0c
blackpirateapps <github@blackpiratex.com>
2023-06-20 20:42:49 +0530

    init commit

--- webring.js
+++ webring.js
@@ -1,15 +1,22 @@
 const websites = [
-    "https://website1.com",
+    "https://firstwebsite.com",
     "https://webring-blue.vercel.app/",
-    "https://webring.blackpiratex.com",
-    "https://website3.com"
+    "https://thirdwebsite.com"
   ];
-
-  const previousLink = document.getElementById('previousLink');
-  const nextLink = document.getElementById('nextLink');
-  const currentWebsite = window.location.href;
-
-  const currentIndex = websites.indexOf(currentWebsite);
-
-  previousLink.href = websites[currentIndex === 0 ? websites.length - 1 : currentIndex - 1];
-  nextLink.href = websites[(currentIndex + 1) % websites.length];
+  
+  const currentWebsite = window.location.hostname;
+  const urlParams = new URLSearchParams(window.location.search);
+  const queryParam = urlParams.get('next');
+  
+  if (queryParam === 'true') {
+    const currentIndex = websites.indexOf(currentWebsite);
+    const nextIndex = (currentIndex + 1) % websites.length;
+    window.location.href = `https://mywebring.com/${websites[nextIndex]}?next=true`;
+  } else {
+    const previousLink = document.getElementById('previousLink');
+    const nextLink = document.getElementById('nextLink');
+    const currentIndex = websites.indexOf(currentWebsite);
+    previousLink.href = `https://mywebring.com/${websites[currentIndex === 0 ? websites.length - 1 : currentIndex - 1]}?next=true`;
+    nextLink.href = `https://mywebring.com/${websites[(currentIndex + 1) % websites.length]}?next=true`;
+  }
+  
